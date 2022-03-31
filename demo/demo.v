a9ba5bec67658b86ee1e2a8dd889fc4053840512
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2022-03-31 10:20:35 -0500

    Demo for pull request #6
    Signed-off-by: Teodor-Dumitru Ene <teodord.ene@gmail.com>

--- demo/demo.v
+++ demo/demo.v
@@ -1,14 +1,15 @@
 module demo(cout, sum, a, b, c, d, cin, test);
 
-    input  [15:0] a, b;
+    input  [15:0] a;
+    input  [31:0] b;
     input  [ 7:0] c, d;
     input cin;
     output cout;
-    output [15:0] sum;
-    output [ 8:0] test;
+    output [31:0] sum;
+    output [ 7:0] test;
 
     assign {cout,sum} = a + (* pptrees_alu, pptrees_base="ripple-carry" *) b + cin;
 
-    assign test = c + (* pptrees_alu, pptrees_base="brent-kung" *) d + cin;
+    assign test = c + (* pptrees_alu, pptrees_base="brent-kung" *) d;
 
 endmodule
